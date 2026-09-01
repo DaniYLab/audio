06b0ac1b7bc8987a2e3b05ea132ec6348911eebd
Darker2003 <datmaziu2003@gmail.com>
2026-09-01 14:10:08 +0700

    chore: remove stray temp file _tmp_ls.py

--- _tmp_ls.py
+++
@@ -1,6 +0,0 @@
-import os
-root = r"D:\Dat\POC\audio\src"
-for base, dirs, files in os.walk(root):
-    for f in sorted(files):
-        if f.endswith(".py") and "cache" not in base:
-            print(os.path.relpath(os.path.join(base, f), root))
